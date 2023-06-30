bf8c578beed62141bdcae9c9437594bbaaaf83b6
kulaginDV <kulagin1104@yandex.ru>
2023-06-30 08:24:16 +0300

    dicision task34

--- task34/Program.cs
+++ task34/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 34. Задайте массив заполненный случайными положительными 
+трёхзначными числами. Напишите программу, которая покажет 
+количество чётных чисел в массиве
+*/
+
+
+void FillArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+
+void PrintArray(int[] numbers)
+{
+        for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+Console.Write("Input size array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArray(numbers);
+Console.WriteLine("Created array: ");
+PrintArray(numbers);
+int count = 0;
+
+for (int j = 0; j < numbers.Length; j++)
+if (numbers[j] % 2 == 0)
+count++;
+
+Console.WriteLine($"Total numbers in the array: {numbers.Length}");
+Console.WriteLine($"Number of even numbers: {count}");
